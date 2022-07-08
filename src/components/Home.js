3d282527a7c51b599a0e6bcc562f96e9a9f1be4a
Natalia Reyveys <reyveysnataly@gmail.com>
2022-07-08 15:39:54 +0300

    Fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,40 @@
 import React, {useEffect} from 'react';
 
 const Home = () => {
+    const addScript = ({ src, id, onLoad }) => {
+        const existing = document.getElementById(id);
+        if (existing) {
+            return existing;
+        } else {
+            const script = document.createElement("script");
+            script.src = src;
+            script.id = id;
+            script.async = true;
+            script.onload = () => {
+                if (onLoad) {
+                    onLoad();
+                }
+            };
+            document.body.appendChild(script);
+            return script;
+        }
+    };
+
+    const removeScript = ({ id }) => {
+        const script = document.getElementById(id);
+        if (script) {
+            document.body.removeChild(script);
+        }
+    };
+
+    useEffect(() => {
+        const script = addScript({
+            src: `https://staging.terracycle.com/en-US/sdk_v2.js`,
+            id: "terracycle"
+        });
+        return () => removeScript({ id: script.id });
+    }, [])
+
     return (
         <div className="row">
             <div className="col-12">
