45fae3221f10782e22e7104d0eafced79f4a1c76
Natalia Reyveys <reyveysnataly@gmail.com>
2022-07-08 15:38:47 +0300

    Add example OCSL widget

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,7 @@ const Home = () => {
                 <p>
                     This is a sample Home page just to demonstrate routing demo
                 </p>
+                <div className="terracycle-widget-new" data-type="ocsl" data-resource-id="105d3960-67e1-4b8d-8038-006705393da1"></div>
             </div>
         </div>
 
