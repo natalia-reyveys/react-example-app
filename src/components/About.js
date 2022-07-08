91d8ceddd3749211d954e9fc6d76e040f3292ab7
Natalia Reyveys <reyveysnataly@gmail.com>
2022-07-08 15:34:27 +0300

    Initial commit

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+
+    return (
+        <div className="row">
+            <div className="col-12">
+                <p>
+                    This is a sample about page just to demonstrate routing demo
+                </p>
+            </div>
+        </div>
+    );
+
+}
+
+export { About };
---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React, {useEffect} from 'react';
+
+const Home = () => {
+    return (
+        <div className="row">
+            <div className="col-12">
+                <p>
+                    This is a sample Home page just to demonstrate routing demo
+                </p>
+            </div>
+        </div>
+
+    );
+}
+
+
+export { Home };
